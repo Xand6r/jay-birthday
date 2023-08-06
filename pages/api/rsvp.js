a388f330c777b513ee4811f52e6a9aaec3e6098b
xander <shuaibualexander@gmail.com>
2023-08-06 16:55:17 +0100

    :fix cors errror

--- pages/api/rsvp.js
+++ pages/api/rsvp.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     });
     console.log({ response });
 
-    res.status(200).json({ user: response });
+    return res.status(200).json({ user: response });
   }
   // if they have ignore
   // otherwise add then
