a455f2c16370b3c6c2886147ec8e3df5b3f35441
xander <shuaibualexander@gmail.com>
2023-08-06 16:44:35 +0100

    :fix cors errror

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const BASE_URL = "https://jay-birthday-27-hfenaqt6e-xand6r.vercel.app/api";
+const BASE_URL = "https://jay-birthday-27.vercel.app/api";
 // const BASE_URL = "http://localhost:3000/api";
 
 export const getRequest = (path) => {
